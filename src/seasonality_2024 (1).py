12c38354a5e991a2fe916865740fbe8dd6b9cad0
giacomo.maggiore <giaco.maggiore@gmail.com>
2024-11-20 15:47:19 +0100

    added helpers seasonality

--- src/seasonality_2024 (1).py
+++ src/seasonality_2024 (1).py
@@ -1,6 +1,8 @@
+
+
 import yfinance as yf
 import pandas as pd
-import numpy
+import numpy as np
 import datetime as dt
 import pandas_datareader.data as pdr
 import seaborn as sns
@@ -11,6 +13,8 @@ ticker = "AAPL"
 
 start = dt.datetime(2020,1,1)
 end = dt.datetime.now()
+
+# DOWNLOAD DATA
 
 def download_data(start, end, ticker):
   date_range = pd.date_range(start=start, end=end, freq="D")
@@ -34,6 +38,8 @@ def download_data(start, end, ticker):
   data["Year"] = data.index.year
 
   return data
+
+# STOCK PRICE SEASONALITY
 
 def calculate_seasonality(start, end, ticker):
   data = download_data(start, end, ticker)
@@ -60,14 +66,15 @@ def calculate_seasonality(start, end, ticker):
   df_seasonality.bfill(inplace = True)
   return df_seasonality
 
+# MONTHLY VOLUME SEASONALITY
 
-def volume_seasonality(start, end, ticker):
+def monthly_volume_seasonality(start, end, ticker):
   data = download_data(start, end, ticker)
 
   years = data["Year"].unique()
 
 
-  month_range = numpy.arange(1,13,1)
+  month_range = np.arange(1,13,1)
 
   volume_df = pd.DataFrame(index=month_range, columns=years)
 
@@ -76,3 +83,46 @@ def volume_seasonality(start, end, ticker):
     volume_df[year] = data_year["Volume"].groupby(data_year.index.month).sum()
 
   return volume_df
+
+#MONTHLY VOLATILITY
+
+def monthly_volatility_stagionality(stock, start, end):
+  data = download_data(start, end, stock)
+
+  years = data["Year"].unique()
+
+  month_range = np.arange(1,13,1)
+
+  volatility_df = pd.DataFrame(index=month_range, columns=years)
+  for year in years:
+    data_year = data[data["Year"] == year]
+    volatility_df[year] = data_year["Adj Close"].groupby(data_year.index.month).std()
+
+  return volatility_df
+
+# CUMULATIVE VOLUME SEASONALITY
+
+def cumulative_volume_seasonality(start, end, ticker):
+  data = download_data(start, end, ticker)
+  years = data["Year"].unique()
+
+  #anno bisestile
+  first_day = dt.datetime(2016,1,1)
+  last_day = dt.datetime(2016,12,31)
+
+  one_year_series = pd.date_range(start=first_day, end=last_day, freq="D")
+
+  df_cumulative_volume = pd.DataFrame(index = one_year_series, columns=years)
+  df_cumulative_volume.index = df_cumulative_volume.index.strftime("%m-%d")
+
+  for year in years:
+    data_year = data[data["Year"] == year]
+
+    data_year.index = data_year.index.strftime("%m-%d")
+    data_year["Cumulative Volume"] = data_year["Volume"].cumsum()
+    df_cumulative_volume[year] = data_year["Cumulative Volume"]
+
+
+
+  return df_cumulative_volume
+
