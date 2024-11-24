42142b6827609a494d2a3760df0c30ca553ae84e
giacomo.maggiore <giaco.maggiore@gmail.com>
2024-11-24 17:08:52 +0100

    added docstring in download and seasonality functions

--- src/seasonality_2024 (1).py
+++ src/seasonality_2024 (1).py
@@ -11,47 +11,75 @@ import matplotlib.pyplot as plt
 
 ticker = "AAPL"
 
+ 
+#start date will be given by a parameter and fastAPI
+#!!! important to put JANUARTY 1 as starting date !!!
+
 start = dt.datetime(2020,1,1)
 end = dt.datetime.now()
 
-# DOWNLOAD DATA
-
 def download_data(start, end, ticker):
+  """Download data using yfinance (no need to use premium data provider)
+
+  Args:
+      start (datetime date object): starting period date (must be JANUARY 1)
+      end (datetime date object): ending period date
+      ticker (string): commodity/stock/etf as given in yFinance
+
+  Returns:
+      _type_: pandas dataframe with datetime index and columns ["Adj Close", "Volume", "Year"]
+  """
+  #create date range serie 
   date_range = pd.date_range(start=start, end=end, freq="D")
 
+  #format date range serie in year-month-day
   data = pd.DataFrame(index=date_range)
   data.index = data.index.strftime("%Y-%m-%d")
 
+
   df_stock = pd.DataFrame()
   historical = yf.download(ticker, start, end)
+  
   df_stock["Adj Close"] = historical["Adj Close"]
   df_stock["Volume"] = historical["Volume"]
   df_stock["Year"] = df_stock.index.year
   df_stock.index = df_stock.index.strftime("%Y-%m-%d")
 
-
-
+  #fill return database
   data["Adj Close"] = df_stock["Adj Close"]
   data["Volume"] = df_stock["Volume"]
+  
   data.bfill(inplace=True)
   data.index = pd.to_datetime(data.index)
   data["Year"] = data.index.year
 
   return data
 
-# STOCK PRICE SEASONALITY
-
 def calculate_seasonality(start, end, ticker):
+  """Merge daily returns over one year in given years
+
+  Args:
+      start (datetime time object): starting period date (must be JANUARTY 1)
+      end (datetime time object): ending period date 
+      ticker (string): commodity/stock/etf as given in yFinance
+
+  Returns:
+      pandas dataframe with index month-day and one for each year.
+  """
   data = download_data(start, end, ticker)
 
-  #anno bisestile
+  #first and last day of a bisestile year
   first_day = dt.datetime(2016,1,1)
   last_day = dt.datetime(2016,12,31)
 
+  #create datetime index of a bisesitle year to avoid errors
   one_year_series = pd.date_range(start=first_day, end=last_day, freq="D")
+
 
   years = data["Year"].unique()
   df_seasonality = pd.DataFrame(index = one_year_series, columns=years)
+  
+  #format datetime
   df_seasonality.index = df_seasonality.index.strftime("%m-%d")
 
   for year in years:
@@ -60,9 +88,12 @@ def calculate_seasonality(start, end, ticker):
     data_year.index = data_year.index.strftime("%m-%d")
 
     initial_year_price = data_year.at["01-01", "Adj Close"]
+    
+    #compute daily returns from first price of the year
     data_year["Adj Close"] = ((data_year["Adj Close"] - initial_year_price)/ initial_year_price) * 100
     df_seasonality[year] = data_year["Adj Close"]
 
+  #need to fix this bfill (must be done in the price dataframe, not the return dataframe!!!) 
   df_seasonality.bfill(inplace = True)
   return df_seasonality
 
